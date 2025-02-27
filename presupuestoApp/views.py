421f3911ca28aab1dbf4f55c5a472f9c2fb0eec5
davidArce <davidsteven654@gmail.com>
2025-02-26 22:55:34 -0500

    ventas

--- presupuestoApp/views.py
+++ presupuestoApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Producto
 
 def presupuesto(request):
+    ventas = Producto.objects.values('bod_nom','linea_nom','cliente_grp_nom')[:100]
+    print(ventas)
     return render(request, 'presupuestoApp/presupuesto.html')
