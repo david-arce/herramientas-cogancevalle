4f58740dcc3d7a4504b9e54e1753ba2054bcbbef
davidArce <davidsteven654@gmail.com>
2025-05-13 17:13:05 -0500

    modelos creados

--- carga.py
+++ carga.py
@@ -62,12 +62,56 @@ def cargar_excel_a_postgresql(file_path, sheet_name, db_url, table_name):
     except Exception as e:
         logger.error(f"Error: {e}")
 
+def cargar_excel_a_postgresql_delete(file_path, sheet_name, db_url, table_name):
+    try:
+        # Leer el archivo Excel
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
+        logger.info(f"Hoja '{sheet_name}' del archivo Excel leída correctamente.")
+        
+        # Conectar a PostgreSQL
+        engine = create_engine(db_url)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        
+        # Definir la tabla con SQLAlchemy
+        metadata = MetaData()
+
+        # Crear una estructura de tabla con un campo 'id' como llave primaria
+        tabla = Table(
+            table_name, metadata,
+            Column('id', Integer, primary_key=True),  # Definición del campo id como llave primaria
+            *(Column(col_name, mapear_tipos(df[col_name])) for col_name in df.columns if col_name != 'id')  # Otras columnas
+        )
+        
+        try:
+             # Eliminar la tabla si existe
+            session.execute(text(f"DROP TABLE IF EXISTS {table_name} CASCADE"))
+            session.commit()
+            logger.info(f"Tabla '{table_name}' eliminada si existía.")
+
+            # Crear la tabla en la base de datos
+            metadata.create_all(engine)
+            logger.info(f"Tabla '{table_name}' creada con éxito.")
+
+            # Cargar los datos en la tabla
+            df.to_sql(table_name, engine, if_exists='append', index=False)
+            logger.info("Datos cargados exitosamente en PostgreSQL.")
+        except Exception as e:
+            session.rollback()
+            logger.error(f"Error durante la carga de datos: {e}")
+            raise
+        finally:
+            session.close()
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+
 # Parámetros
 ruta_carpeta = os.path.join('..', 'bd')
-file_path = os.path.join(ruta_carpeta, 'BD.xlsx')
-sheet_name = 'ROTACIÓN'
+file_path = os.path.join(ruta_carpeta, 'PRESUPUESTO.xlsx')
+sheet_name = 'BD'
 db_url = os.getenv('DATABASE_URL')
-table_name = 'demanda'
+table_name = 'bd_presupuesto_1'
 
 # Llamada a la función
-cargar_excel_a_postgresql(file_path, sheet_name, db_url, table_name)
+cargar_excel_a_postgresql_delete(file_path, sheet_name, db_url, table_name)
--- presupuestoApp/models.py
+++ presupuestoApp/models.py
@@ -1,3 +1,10 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 class Producto(models.Model):
@@ -60,3 +67,67 @@ class Producto(models.Model):
 
     def __str__(self):
         return f"Producto {self.numero} - {self.sku_nom}"
+
+class BdPresupuesto1(models.Model):
+    lapso = models.IntegerField(db_column='Lapso', blank=True, null=True)  # Field name made lowercase.
+    centro_de_operacion = models.IntegerField(db_column='Centro de Operacion', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_centro_de_operacion = models.CharField(db_column='Nombre Centro de Operacion', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    cliente = models.CharField(db_column='Cliente', blank=True, null=True)  # Field name made lowercase.
+    nombre_cliente = models.CharField(db_column='Nombre Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    clase_cliente = models.IntegerField(db_column='Clase Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_clase_cliente = models.CharField(db_column='Nombre Clase Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    criterio_item_1 = models.FloatField(db_column='Criterio Item 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_criterio_item_1 = models.CharField(db_column='Nombre Criterio Item 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    item = models.IntegerField(db_column='Item', blank=True, null=True)  # Field name made lowercase.
+    nombre_item = models.CharField(db_column='Nombre Item', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    linea_n1 = models.IntegerField(db_column='Linea N1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_linea_n1 = models.CharField(db_column='Nombre Linea N1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    cantidad_1 = models.FloatField(db_column='Cantidad 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    valor_neto = models.IntegerField(db_column='Valor Neto', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    class Meta:
+        managed = False
+        db_table = 'bd_presupuesto_1'
+
+
+class BdPresupuesto2(models.Model):
+    lapso = models.IntegerField(db_column='Lapso', blank=True, null=True)  # Field name made lowercase.
+    centro_de_operacion = models.IntegerField(db_column='Centro de Operacion', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_centro_de_operacion = models.CharField(db_column='Nombre Centro de Operacion', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    criterio_item_1 = models.IntegerField(db_column='Criterio Item 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_criterio_item_1 = models.CharField(db_column='Nombre Criterio Item 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    item = models.CharField(db_column='Item', blank=True, null=True)  # Field name made lowercase.
+    nombre_item = models.CharField(db_column='Nombre Item', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    linea_n1 = models.IntegerField(db_column='Linea N1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_linea_n1 = models.CharField(db_column='Nombre Linea N1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    cantidad_1 = models.FloatField(db_column='Cantidad 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    valor_neto = models.IntegerField(db_column='Valor Neto', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    valor_costo = models.FloatField(db_column='Valor Costo', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_clase_cliente = models.CharField(db_column='Nombre Clase Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    cliente = models.CharField(db_column='Cliente', blank=True, null=True)  # Field name made lowercase.
+    nombre_cliente = models.CharField(db_column='Nombre Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    class Meta:
+        managed = False
+        db_table = 'bd_presupuesto_2'
+
+class BdPresupuesto3(models.Model):
+    lapso = models.IntegerField(db_column='Lapso', blank=True, null=True)  # Field name made lowercase.
+    centro_de_operacion = models.IntegerField(db_column='Centro de Operacion', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_centro_de_operacion = models.CharField(db_column='Nombre Centro de Operacion', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    cliente = models.CharField(db_column='Cliente', blank=True, null=True)  # Field name made lowercase.
+    nombre_cliente = models.CharField(db_column='Nombre Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    clase_cliente = models.IntegerField(db_column='Clase Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_clase_cliente = models.CharField(db_column='Nombre Clase Cliente', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    criterio_item_1 = models.FloatField(db_column='Criterio Item 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_criterio_item_1 = models.CharField(db_column='Nombre Criterio Item 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    item = models.IntegerField(db_column='Item', blank=True, null=True)  # Field name made lowercase.
+    nombre_item = models.CharField(db_column='Nombre Item', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    linea_n1 = models.IntegerField(db_column='Linea N1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nombre_linea_n1 = models.CharField(db_column='Nombre Linea N1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    cantidad_1 = models.FloatField(db_column='Cantidad 1', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    valor_neto = models.IntegerField(db_column='Valor Neto', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    class Meta:
+        managed = False
+        db_table = 'bd_presupuesto_3'
