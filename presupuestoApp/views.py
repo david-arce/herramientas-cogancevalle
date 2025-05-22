3f1f50a3292fd1ee2f0274a6b23a495ac9577332
davidArce <davidsteven654@gmail.com>
2025-05-22 09:35:48 -0500

    bd centro operacion 1

--- presupuestoApp/views.py
+++ presupuestoApp/views.py
@@ -12,14 +12,17 @@ def presupuesto(request):
     bd2 = BdPresupuesto2.objects.values('nombre_linea_n1', 'lapso').annotate(suma=Sum('valor_neto')).values('nombre_linea_n1','lapso', 'suma')
     bd3 = BdPresupuesto3.objects.values('nombre_linea_n1', 'lapso').annotate(suma=Sum('valor_neto')).values('nombre_linea_n1','lapso', 'suma')
     
-    bd_centro_operacion1 = BdPresupuesto1.objects.values('centro_de_operacion').distinct()
+    bd_centro_operacion1 = BdPresupuesto1.objects.filter(centro_de_operacion=1).values('lapso').annotate(suma=Sum('valor_neto'))
     bd_centro_operacion2 = BdPresupuesto2.objects.values('centro_de_operacion').distinct()
     bd_centro_operacion3 = BdPresupuesto3.objects.values('centro_de_operacion').distinct()
     
-    print(bd_centro_operacion1)
     df1 = pd.DataFrame(list(bd1))
     df2 = pd.DataFrame(list(bd2))
     df3 = pd.DataFrame(list(bd3))
+    
+    df_centro_operacion1 = pd.DataFrame(list(bd_centro_operacion1))
+    
+    print(df_centro_operacion1) 
     # Concatenar los dataframes
     df = pd.concat([df1, df3, df2], ignore_index=True)
     # concatenar yyyy y mm en una sola columna llamada lapso
@@ -28,5 +31,4 @@ def presupuesto(request):
     # df.to_excel('ventas.xlsx', index=False)
     # Convertir a lista de diccionarios para pasar al template
     data = df.to_dict(orient='records')
-    print(df)
     return render(request, 'presupuestoApp/presupuesto.html', {'data': data})
