3fd3cf770f5f015ec3bad8607861e6cc81aa95e1
davidArce <davidsteven654@gmail.com>
2025-06-27 21:19:59 -0500

    signals solucionado

--- pronosticosWebApp/signals.py
+++ pronosticosWebApp/signals.py
@@ -3,13 +3,9 @@ from django.db.backends.signals import connection_created
 from django.dispatch import receiver
 from pronosticosWebApp import views
 
-# Variable global para controlar que la función solo se ejecute una vez
-has_run = False
-
 @receiver(connection_created)
 def ejecutar_funcion(sender, **kwargs):
-    global has_run
-    # Solo ejecuta la función si no ha sido ejecutada antes
-    if not has_run:
-        views.lista_productos()
-        has_run = True
+    # desconectamos el receptor para que no vuelva a dispararse
+    connection_created.disconnect(ejecutar_funcion, sender=sender)
+    # tu llamada inicial
+    views.lista_productos()
