a0e77be1628a369ce3f1dfdcc7992410c6ceafe7
davidArce <davidsteven654@gmail.com>
2025-07-02 21:22:50 -0500

    signals comentado

--- pronosticosWebApp/signals.py
+++ pronosticosWebApp/signals.py
@@ -1,15 +1,15 @@
 # signals.py
-from django.db.backends.signals import connection_created
-from django.dispatch import receiver
-from pronosticosWebApp import views
+# from django.db.backends.signals import connection_created
+# from django.dispatch import receiver
+# from pronosticosWebApp import views
 
-# Variable global para controlar que la función solo se ejecute una vez
-has_run = False
+# # Variable global para controlar que la función solo se ejecute una vez
+# has_run = False
 
-@receiver(connection_created)
-def ejecutar_funcion(sender, **kwargs):
-    global has_run
-    # Solo ejecuta la función si no ha sido ejecutada antes
-    if not has_run:
-        views.lista_productos()
-        has_run = True
+# @receiver(connection_created)
+# def ejecutar_funcion(sender, **kwargs):
+#     global has_run
+#     # Solo ejecuta la función si no ha sido ejecutada antes
+#     if not has_run:
+#         views.lista_productos()
+#         has_run = True
