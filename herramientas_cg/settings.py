83321ca7640328dcc492a96deaec3203cd766325
davidArce <davidsteven654@gmail.com>
2026-04-01 12:32:26 -0500

    debug false

--- herramientas_cg/settings.py
+++ herramientas_cg/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)6)&ci1^y^-tkw5y&c^e6(z%#1+c+t-yph_3*nu0j570ie8279'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'herramientas-cogancevalle.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://herramientas-cogancevalle.up.railway.app']
